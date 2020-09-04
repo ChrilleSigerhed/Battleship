e7cb4bce1cc1df9b044bb9ea36b33a84144c5d7d
omnidream <48985006+omnidream@users.noreply.github.com>
2020-09-04 15:01:42 +0200

    Lagt till try/cach och commit på DbConnection.AddPlayer

--- Sup20_12/DbCRUD/DbConnection.cs
+++ Sup20_12/DbCRUD/DbConnection.cs
@@ -25,14 +25,26 @@ namespace Sup20_12.DbCRUD
 
             using (var conn = new NpgsqlConnection(connectionString))
             {
+                conn.Open();
 
-                using (var command = new NpgsqlCommand(stmt, conn))
+                using (var trans = conn.BeginTransaction()) //För att HELA transaktionen ska köras och godkännas (allt eller inget)
                 {
-                    conn.Open();
-                    command.Parameters.AddWithValue("nickname", myPlayer.Nickname);
-                    int id = (int)command.ExecuteScalar();
-                    myPlayer.Id = id;
-                    return myPlayer;
+                    try
+                    {
+                        using (var command = new NpgsqlCommand(stmt, conn))
+                        {
+                            command.Parameters.AddWithValue("nickname", myPlayer.Nickname);
+                            int id = (int)command.ExecuteScalar();
+                            myPlayer.Id = id;
+                        }
+                        trans.Commit(); //När HELA transaktionen har godkänts så ska den kommittas och gå in i vår db
+                        return myPlayer;
+                    }
+                    catch (PostgresException)
+                    {
+                        trans.Rollback();
+                        throw;
+                    }
                 }
             }
         }
@@ -44,12 +56,12 @@ namespace Sup20_12.DbCRUD
         ///<summary>
         ///Returnerar en lista på alla player objekt i databasen med nickname och id.
         ///</summary>
-        public static IEnumerable<Player> GetPlayers()
+        public static IEnumerable<Player> GetPlayers() //Byt private mot public när denna funktion är klar
         {
             return null;
         }
 
-        public static IEnumerable<Highscore> GetHighscore(int id)
+        private static IEnumerable<Highscore> GetHighscore(int id) //Byt private mot public när denna funktion är klar
         {
             string stmt = $"SELECT id, win, date, numberOfMoves FROM highscore WHERE playerId = {id} ORDER BY numberOfMoves DESC;";
             return null;
