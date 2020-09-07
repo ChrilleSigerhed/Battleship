0956febc820db2758cf70340a0cfed3c2e10321b
omnidream <48985006+omnidream@users.noreply.github.com>
2020-09-07 13:49:25 +0200

    Mer fix och GitTest

--- Sup20_12/MainWindow.xaml.cs
+++ Sup20_12/MainWindow.xaml.cs
@@ -28,7 +28,7 @@ namespace Sup20_12
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            Player myPlayer = new Player("");
+            Player myPlayer = new Player("SirMrNoName");
             myPlayer = DbConnection.AddNewPlayerToDb(myPlayer);
 
             Highscore myHighscore = new Highscore(true, 99, myPlayer.Id);
